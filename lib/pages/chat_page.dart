e941555629991eed16199ca44fc6f7e00c935df0
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2025-07-06 23:49:13 +0530

    Backend is workign fine

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -3,7 +3,7 @@ import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import '../services/app_theme.dart';
 
-const String GEMINI_API_KEY = 'AIzaSyB55-p4bDJoQ_6fclkSCjByc2Zeulhc130';
+const String GEMINI_API_KEY = '';
 
 void main() {
   runApp(const MyApp());
